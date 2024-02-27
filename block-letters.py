4bf490fac738611c4bad995ab500d079888d4d99
Kelly V. Rivas <63203779+kvrivas854@users.noreply.github.com>
2024-02-27 17:19:17 -0500

    Update block-letters.py

--- block-letters.py
+++ block-letters.py
@@ -1,1 +1,12 @@
+# Kelly Rivas. 
+# I love surfboarding, photography, and working out.
+
+
+print("k       k       r r r r ")
+print("k     k         r       r ")
+print("k   k           r       r ")
+print("k k             r r r r ")
+print("k   k           r   r ")
+print("k     k         r     r ")  
+print("k       k       r       r ")
 
